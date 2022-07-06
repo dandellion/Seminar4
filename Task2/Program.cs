ee86823b26fa1773649392a2ccd5009bdd74f15b
Tasa <greatwillow@mail.ru>
2022-07-06 17:07:17 +0300

    second task

---
+++ Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿int GetNumber(string message)
+{
+    int result = 0;
+    string errorMessage = "Вы ввели не число. Введите корректное число.";
+
+    while (true)
+    {
+
+        Console.WriteLine(message);
+
+        if (int.TryParse(Console.ReadLine(), out result))
+            break;
+        else
+        {
+            Console.Clear();
+            Console.WriteLine(errorMessage);
+        }
+    }
+
+    return result;
+}
+int number = GetNumber("Введите число");
+int summ = 0;
+var div = 0;
+while (number>0)
+{
+    div = number%10;
+  summ=summ+div;
+  number=number/10;  
+}
+Console.WriteLine(summ);
