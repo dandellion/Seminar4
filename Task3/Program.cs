c211f11ed4f68b68403e3821d4e873dbbe9145b9
Tasa <greatwillow@mail.ru>
2022-07-11 00:25:43 +0300

    third task

---
+++ Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿int[] GetArray(int number)
+{
+    int[] arr = new int[8];
+    Random randomizer = new Random();
+
+    for (int i = 0; i < 8; i++)
+    {
+        arr[i] = randomizer.Next(-number, number);
+    }
+
+    return arr;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for(int i = 0; i< 8; i++)
+    {   if (i < 7)
+            Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}]");
+    }
+}
+Console.WriteLine("Введите максимально возможное число в массиве");
+int maxNumber = int.Parse(Console.ReadLine());
+int[] array = GetArray(maxNumber);
+PrintArray(array);
